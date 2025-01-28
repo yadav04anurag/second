e27476ea6469a45222d0f4bfc52b0fd0deae1ff1
yadav04anurag <yadavanurag1403@gmail.com>
2025-01-29 00:26:23 +0530

    final

--- main.js
+++ main.js
@@ -13,8 +13,5 @@ console.log("Latest Update");
 console.log(" this is the commit after adding  the upi is added ")
 
 console.log(" this is the another branch upi ")
-<<<<<<< HEAD
 console.log("this is the second commit of the upi")
 console.log("this is the third commit of th upi ")
-=======
->>>>>>> parent of 1d419a8 (upi second commit)
