fd1a8edc1de121b2bed0af24fa3253efa6aafc0d
yadav04anurag <yadavanurag1403@gmail.com>
2025-01-28 23:48:50 +0530

    payment second commit

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ const footer = "Footer added in our website";
 console.log(footer);
 const Payment = "Integrated the payment gateway";
 console.log(Payment);
+const upi = "Integrated the UPI";
+console.log(upi);
