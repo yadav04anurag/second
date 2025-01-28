76c2c4f01fdb053f92d43353e5c442b1125f0405
yadav04anurag <yadavanurag1403@gmail.com>
2025-01-28 23:47:39 +0530

    payment first commit and rebased

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ const login = "Login page added";
 console.log(login); 
 const footer = "Footer added in our website";
 console.log(footer);
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
